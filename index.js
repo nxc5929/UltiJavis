9fdde0d131998cbb1fa0e04ebdfc506c3e29b9cd
Nathan Connor <nathan_connor@ultimatesoftware.com>
2019-05-16 18:56:20 -0400

    Slcak has built in thing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-const https = require('https')
+const { WebClient } = require('@slack/web-api');
 
+const web = new WebClient("xoxb-637835815733-631566328737-BhmZAu8I8qrWcDVAe2HOpiDB");
 var app = express();
 
 app.use(bodyParser.urlencoded({
@@ -9,24 +10,6 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use(bodyParser.json())
-
-const options = {
-  hostname: "slack.com",
-  path: "/api/chat.postMessage",
-  method: 'POST',
-  authorization: "Bearer xoxb-637835815733-631566328737-BhmZAu8I8qrWcDVAe2HOpiDB",
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}
-
-const request = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`)
-
-  res.on('data', (d) => {
-    process.stdout.write(d)
-  })
-})
 
 app.use('/challenge', function (req, res) {
     console.log(req.body)
@@ -36,11 +19,11 @@ app.use('/challenge', function (req, res) {
     }else{
         console.log("REGULAR");
         console.log(req.body.event.text);
-        var data = JSON.stringify({
-            text: 'Buy the milk',
+        var data = {
+            text: 'HELLO WORLD',
             channel: req.bodychannel
-          })
-        request.write(data);
+          }
+        web.chat.postMessage(data);
         res.send("HELLO MY PEEPS!");
     }
 });
