1e6703a445b410ece6a7abfdb6ba810695c025b4
Nathan Connor <nathan_connor@ultimatesoftware.com>
2019-05-16 16:20:11 -0400

    Added HTTP POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+const https = require('https')
 
 var app = express();
 
@@ -9,11 +10,36 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 
+const options = {
+  hostname: 'https://slack.com/api/chat.postMessage',
+  method: 'POST',
+  authorization: "Bearer xoxb-637835815733-631566328737-BhmZAu8I8qrWcDVAe2HOpiDB",
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
+
+const request = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
 app.use('/challenge', function (req, res) {
     console.log(req.body)
     if(req.body.challenge){
+        console.log("SENT CHALLENGE");
         res.send(req.body.challenge);
     }else{
+        console.log("REGULAR");
+        console.log(req.body.event.text);
+        var data = JSON.stringify({
+            text: 'Buy the milk',
+            channel: req.bodychannel
+          })
+        request.write(data);
         res.send("HELLO MY PEEPS!");
     }
 });
