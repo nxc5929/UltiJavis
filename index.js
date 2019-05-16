1ba4bc0433f505566b8e4517fc1e0a34386d178b
Nathan Connor <nathan_connor@ultimatesoftware.com>
2019-05-16 15:36:23 -0400

    Added Procfile

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.use('/findFood', function (req, res) {
+    console.log(req.body)
+    res.send('Hello World!');
+  });
+
+app.use('/challenge', function (req, res) {
+    console.log(req)
+    console.log(req.body.challenge)
+    res.send(req.body.challenge);
 });
 
 app.listen(8080, function () {
